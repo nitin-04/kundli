fc7f7a4b8ce4d74322c037b9223a13117b607981
Nitin <nitinbahuguna1251@gmail.com>
2025-11-28 22:08:25 +0530

    fix: update path resolution for serving index.html in server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ async function getCoordinates(city) {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve('public/index.html'));
 });
 
 app.post('/generate', async (req, res) => {
